fb5f67b7e99e5bd8af016121e435ef80b64c7973
Jacob David Diehl <jakediehl17@gmail.com>
2023-01-30 15:02:38 -0700

    commit tree 75e0537b2f6e102afedf20d425f7f2f8e454fbc9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+def initialize(brand)
+    self.brand = brand
+end
+def cobble
+"the shoe has been repaired" 
+shoe.condition = "new"
+# Shoe.condition = "new"
+puts ("Your shoe is as good as new!")
+end
+
+end
+
+
